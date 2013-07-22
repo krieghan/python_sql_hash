a891256b186d8f03e09f372608117018cdba61d3
Krieghan Riley <krieghan.riley@gmail.com>
2013-07-22 15:32:20 -0700

    Got simple where conditions working

--- sqlhash.py
+++ sqlhash.py
@@ -11,17 +11,33 @@ def execute(query):
   token_types = get_token_types(tokens) 
   if token_types[0] == "Token.Keyword.DML":
     if tokens[0].value.lower() == "select":
-      #SELECT * FROM table
-      if token_types == ['Token.Keyword.DML', 'Token.Wildcard', 'Token.Keyword', 'Identifier']:
-        table_name = tokens[3].value
-        return state[table_name]
-      #SELECT column FROM table
-      elif token_types == ['Token.Keyword.DML', 'Identifier', 'Token.Keyword', 'Identifier']:
-        table_name = tokens[3].value
-        column_name = tokens[1].value
-        return [{column_name : row[column_name]} for row in state[table_name]]
-      else:
-        raise QueryGrammarNotRecognized(token_types)
+      select_query = SelectQuery()
+      select_query.to_project = [tokens[1].value]
+      select_query.source = tokens[3].value
+      select_query.conditions = []
+      where = [x for x in tokens if x.__class__.__name__ == "Where"]
+      if len(where) > 0:
+        select_query.conditions = []
+        for condition in list(where[0].get_sublists()):
+          select_query.conditions.append([x for x in condition.flatten() if not x.is_whitespace()])
+      rows = state[select_query.source]
+      selected_rows = []
+      for row in rows:
+        match_failed = False
+        for condition in select_query.conditions:
+          if row[condition[0].value] != condition[2].value.replace("'", ""):
+            match_failed = True
+            break
+        if not match_failed:
+          selected_rows.append(row)
+
+      projected_rows = [] 
+      for row in selected_rows:
+        projected_row = dict((column_name, row[column_name]) for column_name in select_query.to_project)
+        projected_rows.append(projected_row)
+
+      return projected_rows
+
     else:
       raise QueryGrammarNotRecognized(token_types)
   else:
@@ -37,6 +53,13 @@ def get_token_types(tokens):
   return token_types
 #  import pdb; pdb.set_trace()
 #  print token_types
+
+class SelectQuery(object):
+  def __init__(self):
+    self.to_project = None
+    self.source = None
+    self.conditions = []
+
 
 class QueryGrammarNotRecognized(Exception):
   pass
